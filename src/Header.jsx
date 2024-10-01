f1b2566760c13f763867fd65ddc1ba7711a45815
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-10-01 20:54:48 +0200

    modified:   src/Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header(props) {  
     return (<div id="cabecera">
       <img className="logo" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM3Ua2zNoteZyXj7cUJZRXdMFKC31DsAaZrg&s"} alt="logo" />
-      <h3 id="mensaje">Bienvenido a la página de Lucia Martinez</h3>      
+      <h3 className="mensaje">Bienvenido a la página de Lucia Martinez</h3>      
     </div>)
   }
