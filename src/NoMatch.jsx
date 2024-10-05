642a19968b86d5b5f80e1c649cc6d76b36f05e0d
lumarseijas <lucia.mseijas@alumnos.upm.es>
2024-10-05 19:37:00 +0200

    modified:   src/NoMatch.jsx

--- src/NoMatch.jsx
+++ src/NoMatch.jsx
@@ -1,6 +1,7 @@
+import { useNavigate } from 'react-router-dom';
 export default function NoMatch(props) {
     const navigate = useNavigate();
-    
+
     const handleVolver = () => {
         navigate('/');
     };
